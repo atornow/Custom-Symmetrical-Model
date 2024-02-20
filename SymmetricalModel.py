78b10e70d7721d7c13a3d82a9a6273d9a5a19f44
atornow <atornow22@gmail.com>
2024-02-20 15:42:36 -0800

    Moved Case/Desired generation logic into ModelFunctions, functions adjusted in accordance.

--- SymmetricalModel.py
+++ SymmetricalModel.py
@@ -3,34 +3,17 @@ import ModelFunctions
 
 # Hyper parameters of the model
 modelSize = 10
-learningRate = 0.05
-momentum = 0.8
-epochs = 1000
-fullRunCount = 5
+learningRate = 0.25
+momentum = 0.9
+epochs = 2000
 genData = False
-genValid = False
-genWeights = False
-train = True
-vErrorSet = [0.0] * fullRunCount
-Cases, Desired, validationCases, validationDesired = ModelFunctions.load_or_generate_data(100, genData, genValid)
+genValid = True
+genWeights = True
 
-if train:
+# See how same validation set error after training decreases as chosen variable changes
+ErrorSet, vErrorSet = ModelFunctions.runEpochs(epochs, momentum, learningRate, genWeights, genData, genValid, modelSize)
 
-    # See how same validation set error after training decreases as chosen variable changes
-    for i in range(0, fullRunCount):
-        learningRate += 0.05
-        ErrorSetEpoch = ModelFunctions.runEpochs(epochs, Cases, Desired, momentum, learningRate, genWeights, modelSize)
-
-        # Perform forward pass for each vCase using learned weights
-        vCaseIndex = 0
-        for vCase in validationCases:
-            Xa, Ya, Xb, Yb, Xz, Yz, weightIndex, error = ModelFunctions.forwardPass(np.load('model_weights.npy'),
-                                                                                    validationDesired[vCaseIndex], vCase
-                                                                                    )
-
-            # Sum error over the set and save as index in main validation error set
-            vErrorSet[i] += np.square(error)
-
-ModelFunctions.plot_error_set(vErrorSet)
 print(np.load('model_weights.npy'))
+ModelFunctions.plot_error_set(vErrorSet)
+
 
